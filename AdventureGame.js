9f9fef639172d3b5eb845a0d10246e0f4d28ae05
Donna <d@home.com>
2025-11-23 18:22:46 +0000

    Initial commit - character stats setup

---
+++ AdventureGame.js
@@ -0,0 +1,20 @@
+const readline = require('readline-sync');
+
+/*
+Adventure Game
+This game will be a text-based game where the player will be able
+to make choices that affect the outcome of the game.
+The player will be able to choose their own path and the story will change
+based on their decisions.
+*/
+
+// Display the game title
+console.log("Welcome to the Adventure Game");
+
+// Add a welcome message
+console.log("Prepare yourself for an epic journey!");
+
+let playerName = '';
+// Get player name using readline-sync
+playerName = readline.question('Enter the player name: ');
+console.log('Player name: ' + playerName);
